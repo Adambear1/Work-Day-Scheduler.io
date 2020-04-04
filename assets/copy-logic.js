484728b7baabf85dd10d75f659762100c9721aa0
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-04-04 14:02:51 -0700

    updated

--- assets/copy-logic.js
+++ assets/copy-logic.js
@@ -1,3 +1,4 @@
+//Variables
 //EXAMPLE FORMAT:: const hour = moment().format('HH:mm:ss')
 var hour = document.querySelector(".hour");
 var task = document.querySelectorAll(".task");
@@ -19,59 +20,20 @@ var closeBtn = document.querySelector(".fa-times")
 var local = document.querySelector(".location")
 var popup = document.querySelector(".bg-modal")
 
-console.log(task)
-console.log(img)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////// AESTHETICS & INTERFACE APIS ////////////////////////////////////////////////////////////////////////////
+
+//Displays date on HTML automatically through API
 function todaysDate(){
     var today = moment().format("dddd, MMMM Do YYYY");
+    //Concatinate by adding calendar image next to it
     todayDisplay.innerHTML = today + "            " + '<i class="fas fa-calendar-alt" style="font-size: 20px">';
-    console.log(today)
 }
 todaysDate()
 
-
-var arrayRaw = localStorage.getItem("tasks")
-var arrayData = JSON.parse(arrayRaw)
-console.log(arrayData)
-// console.log(arrayData.length);
-// window.load()
-
-
-if(arrayData != null){
-    for (i = 0; i < arrayData.length; i++) {
-        if(arrayData[i].planner != null){
-
-            
-            var inputNumber = arrayData[i].id
-            var interval = parseInt(inputNumber.substring(0, inputNumber.length - 2))
-            console.log(interval);
-            if (interval < 6){
-                var newInterval = interval + 12
-            }
-            console.log(newInterval)
-            console.log(hourTime);
-            var inputData = document.createElement("div");
-            inputData.classList.add("task");
-            if (newInterval < hourTime){
-                inputData.classList.add("submit-approval-past");
-            }
-            else if (newInterval === hourTime){
-                inputData.classList.add("submit-approval-present");
-            }
-            else {
-                inputData.classList.add("submit-approval-future");
-            }
-            
-            inputData.classList.add("displayText");
-            inputData.textContent = (arrayData[i].planner);
-            var test = "#input" + arrayData[i].id;
-            $(test).replaceWith(inputData);
-        }
-    }
-}
-
-// Location
+// Using weather API and built in functions within JavaScript to identify lat and lon coordinations for weather updates + city/state identification.
 function getLocation() {
-    // Make sure browser supports this feature
+    // Coniditional statement to make sure browser supports geolocating feature
     if (navigator.geolocation) {
       // Provide our showPosition() function to getCurrentPosition
       navigator.geolocation.getCurrentPosition(showPosition);
@@ -81,42 +43,62 @@ function getLocation() {
     }
   }
 
-  // This will get called after getCurrentPosition()
+  // Called after the getLocation function and takes an argument of position.
   function showPosition(position) {
-    // Grab coordinates from the given object
+    // Grab coordinates from the given object & rounds off to nearest 10th.
     var lat = Math.round(position.coords.latitude * 10)/10;
     var lon = Math.round(position.coords.longitude * 10)/10;
-    console.log("Your coordinates are Latitude: " + lat + " Longitude " + lon);
-
     // Call our next function, passing on the coordinates
     weatherDisplay(lat, lon);
   }
+//Calls function of getLocation, where showPosition is nested/called in
 getLocation();
 
+
+//Function to change background color task input section
 function backGroundColor(){
     for (var i = 0; i < input.length; i++) {
-        console.log(input)
-        console.log(input[i])
-        console.log(input[i].id)
-        console.log(img[i])
-        console.log(img[i].classList[1])
+        //Follows similar conditional statement to see if id of input value is less than curent time.
+        //If so, then the background color is grey, to show that the time has passed.
         if (input[i].id < hourTime) {
             input[i].setAttribute("style", "background-color: lightslategray; opacity: 50%");
-            // img[i].classList[1].setAttribute("style", "color: lightslategray; opacity: 50% !important"); 
-            console.log(task[i])
             }
+        //If time === current time, then red
         else if (input[i].id == hourTime) {
             input[i].setAttribute("style", "background-color: red; opacity: 50%");
         }
+        //If neither, then must be in future and is green
         else {
             input[i].setAttribute("style", "background-color: green; opacity: 50%");        
         }
     }}
-console.log(input)
-console.log(input.length)
+
 backGroundColor();
+
+
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////    BUTTON FUNCTIONS    ///////////////////////////////////////////////////////////////
+
+
+//Buttons 9AM - 5PM all follow same business logic. 
+//Starts by setting variables through moment.js as well as setting an empty array named tasks 
+//Each button has independent function that is then called within button click in succeeding time.
+//The function grabs the array and adds two names with paired keys.
+                            //names/(keys) = id (time of input)
+                            //               planner (value of inputs)
+//Once array is called and added to within function, it is then pushed to global value to add to.
+//The function of localStorage.setItem is then called to store that array with all functions added.
+//Then conditional formatting is called that gives each task input an effect to acknowledge to user that task was saved.
+                            //Follows same logic as before, where if ID < Time, then it is in past & colored grey....... etc.
+//Following the conclusion of each function is an event listener for onclick. It states that once the DOM Content is Loaded, meaning all stored values of text
+//are registeredto the DOM, then the preceeding function is called, which stores the values and gives the effect. 
+                            //Essentially, it prevents an input from not being saved, if for some reason there is a lag within.
+
+
+
+
 const day = moment().format('dddd, MMMM Do YYYY');
 const month = moment().format('MMMM YYYY')
 let tasks  = []
@@ -327,26 +309,89 @@ const addTask17 = (event)=>{
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#button17').addEventListener('click', addTask17)
 })
-/////////////////////////////////////////////////////  POPUP CALENDAR  ///////////////////////////////////////////////////////////////////////
+
+//Retrieving Stored Tasks & Displaying After Refresh
+var arrayRaw = localStorage.getItem("tasks")
+var arrayData = JSON.parse(arrayRaw)
+//Conditional statement so only stored values are displayed
+if(arrayData != null){
+    for (i = 0; i < arrayData.length; i++) {
+        //'Planner' is a key within the object. Holds value of 'task input'
+        if(arrayData[i].planner != null){
+            //Pulled string from local storage, parsed it, and takes id value, which ranges from 0-9 (9AM-5PM)
+            var inputNumber = arrayData[i].id
+            //Trims last two digits from returned string (AM or PM) and changes into intiger for comparision with current time.
+            var interval = parseInt(inputNumber.substring(0, inputNumber.length - 2))
+            //Creating variable for div to house retrieved values within task class.
+            var inputData = document.createElement("div");
+            //Adding css class of task to the new div to assure it takes up 100% of fill.
+            inputData.classList.add("task");
+            
+            //If returned time is less than 6, then adding 12 hours because all hours retrieved from moment.JS are in military standard (0-24).
+            //Essentially shown to convert so all PM hours are converted and stored into same measurement standards.
+            //Must be displayed within function because a new local variable is being created based off such measures, so simply this function nests all conditional statements
+            //To make sure that once noon hits, then 12 is added to each succeeding number to equate properly.
+            function int (){
+                if (interval < 6){
+                    var newInterval = interval + 12
+            }
+                    //Conditional statement to compare returned value of input to current time. 
+                    //Logic that follows is that IF retrieved value is less than current time, THEN the retrieved value MUST be less conditionally formatted to appropriate color.
+                    if (newInterval < hourTime){
+                        //Since ID < time, then adding class of 'past' to assure colors are coordinated.
+                        inputData.classList.add("submit-approval-past");
+                    }
+                    else if (newInterval === hourTime){
+                        //ID = time, color will therefore be red
+                        inputData.classList.add("submit-approval-present");
+                    }
+                    else {
+                        //ID is greater than time, therefore it is in the future and will be green.
+                        inputData.classList.add("submit-approval-future");
+                    }
+        }
+        int() 
+            //Now that Conditional formatting is sufficied, we add class list of 'displayText'
+            inputData.classList.add("displayText");
+            //Adds the value of retrieved object to the text box for display within new Div (reference line 45)
+            inputData.textContent = (arrayData[i].planner);
+            //Using jQuery to dynamically identify which value is retireved and to find coordinating input value through concatination of for loop variable to replace with new value.
+            var test = "#input" + arrayData[i].id;
+            $(test).replaceWith(inputData);
+           
+        }
+    }
+}
 
 
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////  POPUP MODAL///////////////////////////////////////////////////////////////////////
+
+
+//Function to make popup appear when date/time is clicked
 todayDisplay.addEventListener("click", function(){
     popup.classList.remove('hide');
     container.classList.add('hide');
 
 })
 
-
+//closes out of modal when close button is clicked
 closeBtn.addEventListener("click", function(){
     popup.classList.add('hide');
     container.classList.remove('hide')
 
 })
 
+// //Adds retrieved string stored to summary sheet in correct time frame.
 // for (var j = 0; j < 9; j++) {
+//         //Calls locally the retrieved items and parses into an object
 //         var taskRAW = JSON.parse(localStorage.getItem('tasks'))
-//         // console.log(taskRAW[0].planner)
-//         // var task = taskRAW[0].id.substr(-3)
+//         console.log(taskRAW[0].planner)
+//         var task = taskRAW[0].id.substr(-3)
 //             for(var i = 9; i < 18; i++){
 //                 // console.log(document.querySelector("#hour"+[i]).textContent);
 //                 if (taskRAW[j].id.substr(-3) === document.querySelector("#hour"+[i]).textContent){
@@ -358,121 +403,52 @@ closeBtn.addEventListener("click", function(){
 //             }
 //         }
     
-    
-//     var tasknineRAW = JSON.parse(localStorage.getItem('tasks'))
-//     var tasknine = tasknineRAW[0].id.substr(-3)
-//     // var tasknine = tasknineRAW.substr(-3)
-    
-//     console.log(tasknine)
 
 
-
-
-// 
-
-function backGroundColor(){
-for (var i = 0; i < input.length; i++) {
-    console.log(input)
-    console.log(input[i])
-    console.log(input[i].id)
-    console.log(img[i])
-    console.log(img[i].classList[1])
-    
-    if (input[i].id < hourTime) {
-        input[i].setAttribute("style", "background-color: lightslategray; opacity: 50%");
-        // img[i].classList[1].setAttribute("style", "color: lightslategray; opacity: 50% !important"); 
-        console.log(task[i])
-        }
-    else if (input[i].id == hourTime) {
-        input[i].setAttribute("style", "background-color: red; opacity: 50%");
-    }
-    else {
-        input[i].setAttribute("style", "background-color: green; opacity: 50%");        
-    }
-}}
-   
-console.log(input)
-console.log(input.length)
-
-backGroundColor();
-
-
-
-function getLocation() {
-// Make sure browser supports this feature
-if (navigator.geolocation) {
-  // Provide our showPosition() function to getCurrentPosition
-  navigator.geolocation.getCurrentPosition(showPosition);
-} 
-else {
-  alert("Geolocation is not supported by this browser.");
-}
-}
-// This will get called after getCurrentPosition()
-function showPosition(position) {
-// Grab coordinates from the given object
-var lat = Math.round(position.coords.latitude * 10)/10;
-var lon = Math.round(position.coords.longitude * 10)/10;
-console.log("Your coordinates are Latitude: " + lat + " Longitude " + lon);
-
-// Call our next function, passing on the coordinates
-weatherDisplay(lat, lon);
-
-}
-getLocation();
-
-
-// 
-
-//Weather API
+//Weather API used for dynamic background
 function weatherDisplay(lat, lon) {
-// var city = "University+Place"
-// var latitude =   ;
-// var longitude =    ;
-
+//Takes defined lat & long for each used based off their current location & concatinates into variable URL to be used in weather API
 var queryURL = 'http://api.openweathermap.org/data/2.5/find?lat='+ lat + '&lon=' +  lon + '&APPID=79bb7dc0e8f07f6ebe01166410e6e392&APPID=79bb7dc0e8f07f6ebe01166410e6e392'
-// var queryURLCity = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=79bb7dc0e8f07f6ebe01166410e6e392"
-console.log(queryURL)
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response){
-    console.log(response)
-    console.log(response.list[0].name)
-    console.log(response.list[0].main.temp)
+    //Retrieves current City that user is in & Kelvin recorded for that location and stores in variables.
     var City = response.list[0].name;
     var Kelvin = (response.list[0].main.temp)
-    // var Kelvin = parseInt(response.main.temp)
+    // Converts Kelvin to Farenheir
     var Farenheit = Math.floor((Kelvin - 273.15) * 9/5 + 32)
-    console.log(Farenheit)
+    //Adds to HTML for display
     weather.textContent = Farenheit + "°"
     local.textContent = City + ", WA"
-
-    console.log(response)
-
+    //Identifies forecast to make background of summary sheet dynamic as well to display what the current weather is.
     var forecast = response.list[0].weather[0].main
-    console.log(forecast);
-    console.log(response.list[0].weather[0].main)
+    //Conditional statement to show that if it is before 6 AM or after 8 PM, then it is nighttime, and does not check for weather.
+    //However, if before 6AM and 8PM, then checks for Clouds
     if(hourTime > 6 && hourTime < 20 && forecast === "Clouds"){
         //Cloudy pic
         document.querySelector('.modal-content').setAttribute("style", "background-image: url('https://c1.wallpaperflare.com/preview/399/588/997/air-sky-cloud-background.jpg')")
         }
+    //If not clouds, then checks for rain
     else if (hourTime > 6 && hourTime < 20 && forecast === "Rain"){
         //Rainy pic
         document.querySelector('.modal-content').setAttribute("style", "background-image: url('https://res.cloudinary.com/twenty20/private_images/t_watermark-criss-cross-10/v1563556593000/photosp/1ff6c1b0-139b-42c2-b1af-a6096aa3d847/stock-photo-water-wet-raindrop-glass-liquid-moist-surface-aqua-background-1ff6c1b0-139b-42c2-b1af-a6096aa3d847.jpg')")
         }
+    //If not rain or clouds,  then checks for clear skies
     else if (hourTime > 6 && hourTime < 20 && forecast === "Clear"){
         //Sunny pic
         document.querySelector('.modal-content').setAttribute("style", "background-image: url('https://wallpaperplay.com/walls/full/5/8/5/76997.jpg')")  
         }
+    //If not, then checks for mist
+    else if (hourTime > 6 && hourTime < 20 && forecast === "Mist"){
+        //Sunny pic
+        document.querySelector('.modal-content').setAttribute("style", "background-image: url('https://previews.123rf.com/images/leoco/leoco1612/leoco161200074/69459908-surreal-figures-on-a-misty-background.jpg')")  
+        }
+    //If none, then defaults to nighttime
     else {
          //Night pic
          document.querySelector('.modal-content').setAttribute("style", "background-image: url('https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/ug37e-PZBJNCA7CB8-Full-Image_GalleryBackground-en-US-1522432141586._SX1080_.jpg')") 
         }
-
-    
-
-    console.log(forecast == 'Clear')
 })
 }
 
